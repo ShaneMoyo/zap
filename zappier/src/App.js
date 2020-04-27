3dda5bf56102d4b3cb3944f59be0b2ec644c67e0
Shane Moyo <smoyo@cvent.com>
2020-04-27 08:27:13 -0700

    getting gists.

--- zappier/src/App.js
+++ zappier/src/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [gists, setGists] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleUsernameChnage = value => {
+    console.log('value: ' , value)
+    setUsername(value)
+  }
+
+  const handleSubmit = () => {
+    console.log('submit username: ' , username);
+    setLoading(true);
+    return fetch(`https://api.github.com/users/${username}/gists`)
+      .then(res => {
+        if (res.status !== 200) throw new Error(['Server error.']);;
+        return res.json();
+      })
+      .then( gists => {
+        console.log('gists: ', gists);
+        setGists(gists)
+        setLoading(false);
+        setError('')
+      })
+      .catch(err => {
+        console.log('error: ', err.message);
+        setError(err.message)
+        setLoading(false);
+      })
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" onChange={({ target }) => handleUsernameChnage(target.value)}/>
+        {error && <p>{error}</p>}
+        <button type="button" onClick={() => handleSubmit()}>Get gists</button>
       </header>
     </div>
   );
